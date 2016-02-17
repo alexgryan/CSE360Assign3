2601c313d417f47405e0e4166cb9170e1269f103
Alex Ryan <agryan1@asu.edu>
2016-02-17 08:38:51 -0700

    Initial Check-In for Calculator and CalculatorTest files in Assignment 3 of CSE360.

---
+++ cse360/src/cse360assign3/Calculator.java
@@ -0,0 +1,75 @@
+package cse360assign3;
+
+/** Class performs various arithmetic operations on values given
+ * It can also return the history of those calculations
+ * 
+ * @author Alexander Ryan for CSE360 02/22/2016
+ *
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Creates a new Calculator object and sets total to 0
+	 * 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** 
+	 * getTotal-gives the total after arithmetic operations
+	 * 
+	 * @return current total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add-performs addition with the total and value
+	 * 
+	 * @param value to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract-performs subtraction with the total and value
+	 * 
+	 * @param value to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply-performs multiplication with the total and value
+	 * 
+	 * @param value to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide-performs division with the total and value
+	 * 
+	 * @param value to be divided
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** 
+	 * getHistory-return previous calculation totals from previous arithmetic operations.
+	 * 
+	 * @return previous total values in String form
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
